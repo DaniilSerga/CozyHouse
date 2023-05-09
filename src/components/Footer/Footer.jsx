b15b6260014125cb30cb8ba6edb90d06d112d351
DaniilSerga <daniil.serga@gmail.com>
2023-05-09 14:39:44 +0300

    fixed margins in footer, about section, start section, positioning in help section and highlighting in navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,43 +9,46 @@ const Footer = () => {
     return (
         <div className={classes.footerContainer}>
             <div className={classes.footerWrapper}>
-                <div className={classes.contactsSection}>
-                    <h3 className={classes.title}>For questions <br/> and suggestions</h3>
+                <div className={classes.contentContainer}>
+                    <div className={classes.contactsSection}>
+                        <h3 className={classes.title}>For questions <br /> and suggestions</h3>
 
-                    <div className={classes.mailContainer}>
-                        <div className={classes.iconContainer}>
-                            <img src={mailIcon} alt='Mail'/>
+                        <div className={classes.mailContainer}>
+                            <div className={classes.iconContainer}>
+                                <img src={mailIcon} alt='Mail' />
+                            </div>
+                            <p className={classes.descriptionText}>email@shelter.com</p>
                         </div>
-                        <p className={classes.descriptionText}>email@shelter.com</p>
+
+                        <div className={classes.phoneContainer}>
+                            <div className={classes.iconContainer}>
+                                <img src={phoneIcon} alt='Phone number' />
+                            </div>
+                            <p className={classes.descriptionText}>+13 674 567 75 54</p>
+                        </div>
                     </div>
 
-                    <div className={classes.phoneContainer}>
-                        <div className={classes.iconContainer}>
-                            <img src={phoneIcon} alt='Phone number'/>
+                    <div className={classes.locationsSection}>
+                        <h3 className={classes.title}>We are waiting <br /> for your visit</h3>
+
+                        <div className={classes.locationContainer}>
+                            <div className={classes.iconContainer}>
+                                <img src={pinIcon} alt='Location' />
+                            </div>
+                            <p className={classes.descriptionText}>1 Central Street, Boston <br /> (entrance from the store)</p>
                         </div>
-                        <p className={classes.descriptionText}>+13 674 567 75 54</p>
+
+                        <div className={classes.locationContainer}>
+                            <div className={classes.iconContainer}>
+                                <img src={pinIcon} alt='Location' />
+                            </div>
+                            <p className={classes.descriptionText}>18 South Park, London</p>
+                        </div>
                     </div>
+
+                    <img src={footerImg} alt='Footer section' />
                 </div>
 
-                <div className={classes.locationsSection}>
-                    <h3 className={classes.title}>We are waiting <br/> for your visit</h3>
-
-                    <div className={classes.locationContainer}>
-                        <div className={classes.iconContainer}>
-                            <img src={pinIcon} alt='Location'/>
-                        </div>
-                        <p className={classes.descriptionText}>1 Central Street, Boston <br/> (entrance from the store)</p>
-                    </div>
-
-                    <div className={classes.locationContainer}>
-                        <div className={classes.iconContainer}>
-                            <img src={pinIcon} alt='Location'/>
-                        </div>
-                        <p className={classes.descriptionText}>18 South Park, London</p>
-                    </div>
-                </div>
-
-                <img src={footerImg} alt='Footer section'/>
             </div>
         </div>
     )
